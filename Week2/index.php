6ad53e3bc9048d035d51edb87fc411a527753cd8
kimmygabrys <99371743+kimmygabrys@users.noreply.github.com>
2023-01-23 15:05:57 -0500

    Week 2 Things

---
+++ Week2/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<!--
+Kimmy Gabrys // CIS 2454 // 1/23/23 // Week 2
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo "Hello World from php file";
+        ?>
+    </body>
+</html>
