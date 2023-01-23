22592be39768443470bac5c7dfba72e0c962fa9f
kimmygabrys <99371743+kimmygabrys@users.noreply.github.com>
2023-01-23 14:19:06 -0500

    Lab 1 in Git Intro

--- GitIntro/index.php
+++ GitIntro/index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <!--
-Kimmy Gabrys
+Kimmy Gabrys // CIS 2454 // 1/23/23
 -->
 <html>
     <head>
@@ -9,7 +9,12 @@ Kimmy Gabrys
     </head>
     <body>
         <?php
-        echo "Hello World!";
+        echo "<h2>About Kevin McIlwain</h2>";
+        echo "<p>Currently in his last semester, going for his AAS - Software Engineering Option.</p>";
+        echo "<p>Doesn't have much previous coding experience outside of OCC and an Oracle Apex project he is helping with at work.</p>";
+        echo "<p>His hobbies are making music and real estate investing.</p>";
+        echo "<p>Currently an Audio-Visual tech for a defense company, has a bachelors in music production, and operates a music recording studio with friends</p>";
+        echo "<p>Born and raised on the eastside of Detroit!</p>";
         ?>
     </body>
 </html>
