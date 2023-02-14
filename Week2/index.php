4da9f1b32ff71b11653ccdfaa2399ef0b451c099
kimmygabrys <99371743+kimmygabrys@users.noreply.github.com>
2023-02-13 20:41:05 -0500

    Practice

--- Week2/index.php
+++ Week2/index.php
@@ -1,6 +1,6 @@
 <?php
-    $first_name = htmlspecialchars(filter_input(INPUT_GET, 'first_name'));
-    $last_name = htmlspecialchars(filter_input(INPUT_GET, 'last_name'));
+$first_name = htmlspecialchars(filter_input(INPUT_GET, 'first_name'));
+$last_name = htmlspecialchars(filter_input(INPUT_GET, 'last_name'));
 ?>
 
 <!DOCTYPE html>
@@ -15,9 +15,9 @@ Kimmy Gabrys // CIS 2454 // 1/23/23 // Week 2 Practice
     <body>
 
         <?php include ('nav.php'); ?>
-        
+
         Hello World from php file
-        
+
         <form action="index.php" method="get">
             <label>First name: </label>
             <input type="text" name="first_name"/><br>
@@ -26,7 +26,7 @@ Kimmy Gabrys // CIS 2454 // 1/23/23 // Week 2 Practice
             <label>&nbsp;</label>
             <input type="submit" value="submit" />
         </form>
-        
+
         <form action="view.php" method="post">
             <label>First name: </label>
             <input type="text" name="first_name"/><br>
@@ -35,7 +35,7 @@ Kimmy Gabrys // CIS 2454 // 1/23/23 // Week 2 Practice
             <label>&nbsp;</label>
             <input type="submit" value="submit" />
         </form>
-        
+
         <?php
         echo "Hi " . $first_name . " " . $last_name;
         ?>
